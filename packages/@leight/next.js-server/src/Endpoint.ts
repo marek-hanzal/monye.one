b3c9fc1546eec11394c25b82592f770cc4f4bf06
Marek Hanzal <marek.hanzal@x32.cz>
2023-01-26 20:21:32 +0100

    Fixed build, enabled shared tsconfig for leight

--- packages/@leight/next.js-server/src/Endpoint.ts
+++ packages/@leight/next.js-server/src/Endpoint.ts
@@ -15,60 +15,56 @@ const logger = Logger("@leight/next.js-server");
 
 export const Endpoint =
     <
-            TRequest = unknown,
-            TResponse = unknown,
-            THrefQuery extends IHrefQuery = IHrefQuery
-        >({
-            handler,
-            container,
-            withTokens,
-        }: IHandler<
-            TRequest,
-            TResponse,
-            THrefQuery
-        >): INextHandler<TResponse> =>
-        async (
-            request: NextApiRequest,
-            response: NextApiResponse<TResponse | IError>
-        ) => {
-            try {
-                const $container = container.createChildContainer();
-                const token = await getToken({ req: request });
-                const tokenService = TokenServiceContext($container)
-                    .register((token?.tokens || []) as [])
-                    .resolve();
-                UserIdContext($container).register(token?.sub);
+        TRequest = unknown,
+        TResponse = unknown,
+        THrefQuery extends IHrefQuery = IHrefQuery
+    >({
+        handler,
+        container,
+        withTokens,
+    }: IHandler<TRequest, TResponse, THrefQuery>): INextHandler<TResponse> =>
+    async (
+        request: NextApiRequest,
+        response: NextApiResponse<TResponse | IError>
+    ) => {
+        try {
+            const $container = container.createChildContainer();
+            const token = await getToken({ req: request });
+            const tokenService = TokenServiceContext($container)
+                .register((token?.tokens || []) as [])
+                .resolve();
+            UserIdContext($container).register(token?.sub);
 
-                tokenService.checkAny(withTokens);
+            tokenService.checkAny(withTokens);
 
-                const result = await handler({
-                    container: $container,
-                    tokenService,
-                    userService: UserServiceContext($container).resolve(),
-                    request,
-                    body: request.body,
-                    query: request.query as THrefQuery,
-                    toBody() {
-                        return getRawBody(request);
-                    },
-                    response,
-                    end: response.end,
-                });
+            const result = await handler({
+                container: $container,
+                tokenService,
+                userService: UserServiceContext($container).resolve(),
+                request,
+                body: request.body,
+                query: request.query as THrefQuery,
+                toBody() {
+                    return getRawBody(request);
+                },
+                response,
+                end: response.end,
+            });
 
-                return result && (await response.status(200).json(result));
-            } catch (e) {
-                if (e instanceof TokenError) {
-                    return response
-                        .status(403)
-                        .json({ error: "Token: Access denied." });
-                }
-                if (e instanceof UserError) {
-                    return response
-                        .status(403)
-                        .json({ error: "User: Access denied." });
-                }
-                console.error(e);
-                logger.error(e);
-                return response.status(500).json({ error: "Kaboom!" });
+            return result && (await response.status(200).json(result));
+        } catch (e) {
+            if (e instanceof TokenError) {
+                return response
+                    .status(403)
+                    .json({ error: "Token: Access denied." });
             }
-        };;
+            if (e instanceof UserError) {
+                return response
+                    .status(403)
+                    .json({ error: "User: Access denied." });
+            }
+            console.error(e);
+            logger.error(e);
+            return response.status(500).json({ error: "Kaboom!" });
+        }
+    };
