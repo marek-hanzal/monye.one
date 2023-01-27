2aa5b2b6906e732bde049719ccdcd3cc8c6ff4bf
Marek Hanzal <marek.hanzal@x32.cz>
2023-01-27 20:47:01 +0100

    Ok, procedure wrapper is messing up types, it's time to improve the shit

--- packages/@monye.one/trpc-server/src/import/xlsx/XlsxRouter.ts
+++ packages/@monye.one/trpc-server/src/import/xlsx/XlsxRouter.ts
@@ -1,4 +1,6 @@
-import { XlsxImportProcedure } from "@leight/xlsx-import-server";
+import { ImportServiceContext } from "@leight/xlsx-import-server";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 import { procedure, router } from "../../router";
 
 export const XlsxRouter = router({
@@ -6,5 +8,24 @@ export const XlsxRouter = router({
      * TRY TO USE PROCEDURE DIRECTLY
      */
 
-    job: XlsxImportProcedure(procedure),
+    // job: XlsxImportProcedure(procedure),
+    job: procedure
+        .input(
+            z.object({
+                fileId: z.string(),
+            })
+        )
+        .mutation(async ({ ctx: { container }, input: request }) => {
+            try {
+                // eslint-disable-next-line @typescript-eslint/return-await
+                return await ImportServiceContext(container)
+                    .resolve()
+                    .async(request);
+            } catch (e) {
+                throw new TRPCError({
+                    message: "Could not start an import job. Yaaykes!",
+                    code: "INTERNAL_SERVER_ERROR",
+                });
+            }
+        }),
 });
