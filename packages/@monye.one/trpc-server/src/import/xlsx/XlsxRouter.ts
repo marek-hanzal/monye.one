bd1f99eae8211addc23928a510d0b2793787d1f7
Marek Hanzal <marek.hanzal@x32.cz>
2023-01-27 21:10:07 +0100

    Step by step

--- packages/@monye.one/trpc-server/src/import/xlsx/XlsxRouter.ts
+++ packages/@monye.one/trpc-server/src/import/xlsx/XlsxRouter.ts
@@ -1,21 +1,38 @@
 import { ImportServiceContext } from "@leight/xlsx-import-server";
-import { z } from "zod";
+import { z, ZodType } from "zod";
 import { TRPCError } from "@trpc/server";
+import {
+    DefaultValue as FallbackValue,
+    ResolveOptions,
+} from "@trpc/server/src/core/internals/utils";
+import { MaybePromise } from "@trpc/server/src/types";
+import { ProcedureBuilder } from "@trpc/server/src/core/internals/procedureBuilder";
 import { procedure, router } from "../../router";
 
-export const XlsxRouter = router({
-    /**
-     * TRY TO USE PROCEDURE DIRECTLY
-     */
+type InferProcedureParams<T> = T extends ProcedureBuilder<infer U> ? U : T;
 
+export const withMutation = <
+    TProcedure extends ProcedureBuilder<any>,
+    TRequest extends ZodType,
+    TResponse
+>(
+    request: TRequest,
+    callback: (
+        opts: ResolveOptions<InferProcedureParams<TProcedure>>
+    ) => MaybePromise<
+        FallbackValue<InferProcedureParams<TProcedure>["_output_in"], TResponse>
+    >
+) => {
+    return procedure.input(request).mutation(callback);
+};
+
+export const XlsxRouter = router({
     // job: XlsxImportProcedure(procedure),
-    job: procedure
-        .input(
-            z.object({
-                fileId: z.string(),
-            })
-        )
-        .mutation(async ({ ctx: { container }, input: request }) => {
+    job: withMutation(
+        z.object({
+            fileId: z.string(),
+        }),
+        async ({ ctx: { container }, input: request }) => {
             try {
                 // eslint-disable-next-line @typescript-eslint/return-await
                 return await ImportServiceContext(container)
@@ -27,5 +44,6 @@ export const XlsxRouter = router({
                     code: "INTERNAL_SERVER_ERROR",
                 });
             }
-        }),
+        }
+    ),
 });
