353c7032a26bc861909fb0ace95c57ce714619aa
Marek Hanzal <marek.hanzal@x32.cz>
2023-01-31 16:54:27 +0100

    Optimized package usages, integrated depcheck

--- apps/monye.one/next.config.mjs
+++ apps/monye.one/next.config.mjs
@@ -5,7 +5,9 @@ import i18n from "./next-i18next.config.js";
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (async () => {
+    (await import("./src/env/server.mjs"))
+})();
 
 /** @type {import("next").NextConfig} */
 const config = {
--- packages/@leight/container-server/src/ServerContainer.ts
+++ packages/@leight/container-server/src/ServerContainer.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
-import {FileContainer as $FileContainer, type IFileContainer,} from "@leight/file-server";
-import {type IXlsxImportContainer, XlsxImportContainer as $XlsxImportContainer,} from "@leight/xlsx-import-server";
+import {FileContainer as $FileContainer, type IFileContainer} from "@leight/file-server";
+import {type IXlsxImportContainer, XlsxImportContainer as $XlsxImportContainer} from "@leight/xlsx-import-server";
 import {type IUserContainer, UserContainer as $UserContainer,} from "@leight/user-server";
 import {type IJobContainer, JobContainer as $JobContainer} from "@leight/job-server";
 import {type IImportContainer, ImportContainer as $ImportContainer} from "@leight/import-server";
