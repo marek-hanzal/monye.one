1c828b16690a3f8d74fde95a5e831ad3aa2b56cc
Marek Hanzal <marek.hanzal@x32.cz>
2023-02-06 23:06:41 +0100

    Some experiment

--- packages/@monye.one/transaction-server/src/procedure/TransactionQueryProcedure.ts
+++ packages/@monye.one/transaction-server/src/procedure/TransactionQueryProcedure.ts
@@ -2,6 +2,9 @@ import {type IWithHandler, withHandler} from "@leight/trpc-server";
 import {type ITransactionEntity} from "@monye.one/transaction";
 import {TransactionSourceContext} from "../context";
 
-export const TransactionQueryProcedure: IWithHandler<void, ITransactionEntity[]> = withHandler({
+export interface ITransactionQueryProcedure extends IWithHandler<void, ITransactionEntity[]> {
+}
+
+export const TransactionQueryProcedure: ITransactionQueryProcedure = withHandler({
     handler: async ({container}) => TransactionSourceContext(container).resolve().query(),
 });
