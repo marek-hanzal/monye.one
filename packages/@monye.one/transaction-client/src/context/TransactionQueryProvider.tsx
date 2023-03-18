20d9c2c388692902ee068bdbde6b1da7bbaa45c7
Marek Hanzal <marek.hanzal@x32.cz>
2023-03-18 13:41:50 +0100

    ...

--- packages/@monye.one/transaction-client/src/context/TransactionQueryProvider.tsx
+++ packages/@monye.one/transaction-client/src/context/TransactionQueryProvider.tsx
@@ -1,20 +1,20 @@
-import { type IQueryProviderProps, QueryProvider } from "@leight/query-client";
-import { type ITransactionSourceSchema } from "@monye.one/transaction";
-import { trpc } from "@monye.one/trpc-client";
-import { type FC } from "react";
-import { TransactionSortProvider } from "./TransactionSortProvider";
+import {
+    type IQueryProviderProps,
+    QueryProvider
+}                                      from "@leight/query-client";
+import {type ITransactionSourceSchema} from "@monye.one/transaction";
+import {trpc}                          from "@monye.one/trpc-client";
+import {type FC}                       from "react";
+import {TransactionSortProvider}       from "./TransactionSortProvider";
 
 export interface ITransactionQueryProviderProps
-    extends IQueryProviderProps<ITransactionSourceSchema> {}
+    extends IQueryProviderProps<ITransactionSourceSchema> {
+}
 
-export const TransactionQueryProvider: FC<ITransactionQueryProviderProps> = (
-    props
-) => {
-    return (
-        <QueryProvider
-            useCountQuery={trpc.transaction.source.count.useQuery}
-            SortProvider={TransactionSortProvider}
-            {...props}
-        />
-    );
+export const TransactionQueryProvider: FC<ITransactionQueryProviderProps> = props => {
+    return <QueryProvider<ITransactionSourceSchema>
+        useCountQuery={trpc.transaction.source.count.useQuery}
+        SortProvider={TransactionSortProvider}
+        {...props}
+    />;
 };
--- packages/@monye.one/transaction-client/src/source/TransactionSource.tsx
+++ packages/@monye.one/transaction-client/src/source/TransactionSource.tsx
@@ -1,23 +1,27 @@
-import { type ISourceProps, Source } from "@leight/source-client";
+import {
+    type ISourceProps,
+    Source
+}                from "@leight/source-client";
 import {
     type ITransactionSourceSchema,
     TransactionSchema,
-} from "@monye.one/transaction";
-import { trpc } from "@monye.one/trpc-client";
-import { type FC } from "react";
-import { TransactionProvider, useTransactionSort } from "../context";
+}                from "@monye.one/transaction";
+import {trpc}    from "@monye.one/trpc-client";
+import {type FC} from "react";
+import {
+    TransactionProvider,
+    useTransactionSort
+}                from "../context";
 
-export interface ITransactionSourceProps
-    extends ISourceProps<ITransactionSourceSchema> {}
+export interface ITransactionSourceProps extends ISourceProps<ITransactionSourceSchema> {
+}
 
-export const TransactionSource: FC<ITransactionSourceProps> = (props) => {
-    return (
-        <Source
-            schema={TransactionSchema}
-            SourceProvider={TransactionProvider}
-            useQuery={trpc.transaction.source.query.useQuery}
-            useSortState={useTransactionSort}
-            {...props}
-        />
-    );
+export const TransactionSource: FC<ITransactionSourceProps> = props => {
+    return <Source<ITransactionSourceSchema>
+        schema={TransactionSchema}
+        SourceProvider={TransactionProvider}
+        useSourceQuery={trpc.transaction.source.query.useQuery}
+        useSortState={useTransactionSort}
+        {...props}
+    />;
 };
