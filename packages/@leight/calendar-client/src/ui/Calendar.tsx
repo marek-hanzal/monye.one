fcca2c61c12b26bd0d69c325d1384a36a67ce56d
Marek Hanzal <marek.hanzal@x32.cz>
2023-03-28 15:17:00 +0200

    Preparing calendar stuff

--- packages/@leight/calendar-client/src/ui/Calendar.tsx
+++ packages/@leight/calendar-client/src/ui/Calendar.tsx
@@ -5,16 +5,33 @@ import {
 }                from "@leight/i18n";
 import {Date}    from "@leight/i18n-client";
 import {
+    createStyles,
     Grid,
     Group
 }                from "@mantine/core";
 import {type FC} from "react";
 
+const useStyles = createStyles(theme => ({
+    currentWeek: {
+        backgroundColor: theme.colors["gray"][2],
+    },
+    currentDay:  {
+        backgroundColor: theme.colors["gray"][4],
+    },
+}));
+
 export interface ICalendarProps {
 }
 
 export const Calendar: FC<ICalendarProps> = () => {
-    const {start, end, weeks} = useCalendar({input: DateTime.fromObject({month: 1})});
+    const columnSize = 3;
+    const columns    = (7 * columnSize) + 1;
+    const {classes}  = useStyles();
+
+    const {start, end, weeks} = useCalendar({
+        input: DateTime.now().plus({month: 0}),
+    });
+
     return <>
         Calendar here!
         <Group>
@@ -22,14 +39,19 @@ export const Calendar: FC<ICalendarProps> = () => {
             <span>end: <Date input={end.toJSDate()}/></span>
         </Group>
 
-        <Grid columns={(7 * 3) + 1}>
+        <Grid columns={columns}>
             <Grid.Col span={1}/>
-            {Info.weekdays("long").map(day => <Grid.Col key={`day-${day}`} span={3}>{day}</Grid.Col>)}
+            {Info.weekdays("long").map(day => <Grid.Col
+                key={`day-${day}`}
+                span={columnSize}
+            >
+                <h3>{day}</h3>
+            </Grid.Col>)}
         </Grid>
         {weeks.map(({days, number, current, id}) => <Grid
             key={id}
-            columns={(7 * 3) + 1}
-            style={current ? {backgroundColor: "#EEE"} : undefined}
+            columns={columns}
+            className={current ? classes.currentWeek : undefined}
         >
             <Grid.Col
                 span={1}
@@ -38,8 +60,8 @@ export const Calendar: FC<ICalendarProps> = () => {
             </Grid.Col>
             {days.map(({day, current, id}) => <Grid.Col
                 key={id}
-                span={3}
-                style={current ? {backgroundColor: "#ccc"} : undefined}
+                span={columnSize}
+                className={current ? classes.currentDay : undefined}
             >
                 <Date input={day.toJSDate()}/>
             </Grid.Col>)}
