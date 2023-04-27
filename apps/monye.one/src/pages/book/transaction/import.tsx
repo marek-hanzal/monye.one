e5f8dac25d2416666e39211658ef5010ead0dae6
Marek Hanzal <marek.hanzal@x32.cz>
2023-04-27 21:29:41 +0200

    ...

--- apps/monye.one/src/pages/book/transaction/import.tsx
+++ apps/monye.one/src/pages/book/transaction/import.tsx
@@ -1,12 +1,10 @@
-import logo              from "@/monye.one/assets/logo/logo.svg";
-import {withTranslation} from "@leight/i18n-server";
-import {Paper}           from "@leight/mantine";
-import {Box}             from "@mantine/core";
-import {withBookLayout}  from "@monye.one/book-client";
-import {
-    TransactionImport,
-    TransactionMenu
-}                        from "@monye.one/transaction-client";
+import logo                from "@/monye.one/assets/logo/logo.svg";
+import {withTranslation}   from "@leight/i18n-server";
+import {Paper}             from "@leight/mantine";
+import {Box}               from "@mantine/core";
+import {withBookLayout}    from "@monye.one/book-client";
+import {TransactionMenu}   from "@monye.one/transaction-client";
+import {TransactionImport} from "@monye.one/transaction-ex-client";
 
 export default withBookLayout(
     function Import() {
