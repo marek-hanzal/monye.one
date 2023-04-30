8ab2e9d2a9c3c70f2fb1e8892a358949f5f258ef
Marek Hanzal <marek.hanzal@x32.cz>
2023-05-01 00:42:05 +0200

    ...

--- apps/monye.one/src/pages/book/index.tsx
+++ apps/monye.one/src/pages/book/index.tsx
@@ -1,40 +1,10 @@
-import logo                from "@/monye.one/assets/logo/logo.svg";
-import {withTranslation}   from "@leight/i18n-server";
-import {Button}            from "@mantine/core";
-import {withBookLayout}    from "@monye.one/book-client";
-import {FilterSourceStore} from "@monye.one/filter-client";
+import logo              from "@/monye.one/assets/logo/logo.svg";
+import {withTranslation} from "@leight/i18n-server";
+import {withBookLayout}  from "@monye.one/book-client";
 
 export default withBookLayout(
     function Index() {
-        const foo = FilterSourceStore.use.useCreate();
-
-        return <>
-            <Button
-                onClick={() => {
-                    foo.mutate({
-                        name:   "foo",
-                        type:   "transaction",
-                        filter: {
-                            bla: "1",
-                            ble: 2,
-                        },
-                        dto:    {
-                            ahoj: "nazdar",
-                        },
-                    });
-                    foo.mutate({
-                        name:   "foo2",
-                        type:   "transaction",
-                        filter: {
-                            bla: "23",
-                            ble: 3,
-                        },
-                    });
-                }}
-            >
-                klyk
-            </Button>
-        </>;
+        return null;
     },
     {logo, href: "/book"}
 );
