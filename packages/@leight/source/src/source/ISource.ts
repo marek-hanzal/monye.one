43be7ed9dc2d786a7dad7e2b1afa94f7d829e4dd
Marek Hanzal <marek.hanzal@x32.cz>
2023-05-04 15:01:15 +0200

    Preparing some other quite a refactor

--- packages/@leight/source/src/source/ISource.ts
+++ packages/@leight/source/src/source/ISource.ts
@@ -5,7 +5,7 @@ import {type ISourceSchemaType} from "./ISourceSchemaType";
  * Implementation of data source (general, not just a database one).
  */
 export interface ISource<TSourceSchemaType extends ISourceSchemaType> {
-    create(entity: TSourceSchemaType["Create"]): Promise<TSourceSchemaType["Entity"]>;
+    create(entity: ISource.ICreate<TSourceSchemaType>): Promise<TSourceSchemaType["Entity"]>;
 
     upsert(props: ISource.IUpsert<TSourceSchemaType>): Promise<TSourceSchemaType["Entity"]>;
 
@@ -13,30 +13,32 @@ export interface ISource<TSourceSchemaType extends ISourceSchemaType> {
 
     patchBy(patch: ISource.IPatchBy<TSourceSchemaType>): Promise<unknown>;
 
-    delete(withIdentity: IWithIdentity): Promise<TSourceSchemaType["Entity"]>;
+    delete(withIdentity: ISource.IDelete): Promise<TSourceSchemaType["Entity"]>;
 
-    deleteWith(query: TSourceSchemaType["Query"]): Promise<TSourceSchemaType["Entity"][]>;
+    deleteWith(query: ISource.IDeleteWith<TSourceSchemaType>): Promise<TSourceSchemaType["Entity"][]>;
 
     /**
      * Count items based on an optional query.
      */
-    count(query?: TSourceSchemaType["Query"]): Promise<number>;
+    count(query?: ISource.ICount<TSourceSchemaType>): Promise<number>;
 
     /**
      * Query items.
      */
-    query(query?: TSourceSchemaType["Query"]): Promise<TSourceSchemaType["Entity"][]>;
+    query(query?: ISource.IQuery<TSourceSchemaType>): Promise<TSourceSchemaType["Entity"][]>;
 
-    fetch(query: TSourceSchemaType["Query"]): Promise<TSourceSchemaType["Entity"]>;
+    fetch(query: ISource.IFetch<TSourceSchemaType>): Promise<TSourceSchemaType["Entity"]>;
 
     fetchOptional(query: TSourceSchemaType["Query"]): Promise<TSourceSchemaType["Entity"] | null>;
 
     find(id: string): Promise<TSourceSchemaType["Entity"]>;
 
-    findOptional(id?: string): Promise<TSourceSchemaType["Entity"] | null>;
+    findOptional(id?: string | null): Promise<TSourceSchemaType["Entity"] | null>;
 }
 
 export namespace ISource {
+    export type ICreate<TSourceSchemaType extends ISourceSchemaType> = TSourceSchemaType["Create"];
+
     export interface IPatch<TSourceSchemaType extends ISourceSchemaType> {
         patch: TSourceSchemaType["Patch"];
         filter: TSourceSchemaType["Filter"];
@@ -52,4 +54,14 @@ export namespace ISource {
         patch: TSourceSchemaType["Patch"];
         filter: TSourceSchemaType["Filter"];
     }
+
+    export type IDelete = IWithIdentity;
+
+    export type IDeleteWith<TSourceSchemaType extends ISourceSchemaType> = TSourceSchemaType["Query"];
+
+    export type ICount<TSourceSchemaType extends ISourceSchemaType> = TSourceSchemaType["Query"];
+
+    export type IQuery<TSourceSchemaType extends ISourceSchemaType> = TSourceSchemaType["Query"];
+
+    export type IFetch<TSourceSchemaType extends ISourceSchemaType> = TSourceSchemaType["Query"];
 }
