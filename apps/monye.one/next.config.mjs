82084b4c78e62e21606f44a6502b9d56bb99df59
Marek Hanzal <marek.hanzal@x32.cz>
2023-05-13 11:32:57 +0200

    Build... again... somehow works

--- apps/monye.one/next.config.mjs
+++ apps/monye.one/next.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
-import i18n from "./next-i18next.config.js";
+import i18n from './next-i18next.config.js';
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && await import("./src/env.mjs");
+!process.env.SKIP_ENV_VALIDATION && await import('./src/env.mjs');
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 export default {
     experimental:      {
-        turbo: {},
+        serverActions: true,
+        turbo:         {},
     },
     reactStrictMode:   true,
     swcMinify:         true,
