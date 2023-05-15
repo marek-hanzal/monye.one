607307a2b75c8f45c613a4189528c4785afa046f
Marek Hanzal <marek.hanzal@x32.cz>
2023-05-15 12:12:20 +0200

    ...

--- apps/monye.one/src/app/[locale]/layout.tsx
+++ apps/monye.one/src/app/[locale]/layout.tsx
@@ -1,11 +1,10 @@
-import {locales}                from "@/monye.one/locales";
 import {NextIntlClientProvider} from "next-intl";
 import {notFound}               from "next/navigation";
 import {type PropsWithChildren} from "react";
 
-export function generateStaticParams() {
-    return locales.map(locale => ({locale}));
-}
+// export function generateStaticParams() {
+//     return locales.map(locale => ({locale}));
+// }
 
 export type ILayoutProps = PropsWithChildren<{
     params: {
